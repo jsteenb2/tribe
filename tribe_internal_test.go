2ae7c0ae9b32e99e8835ddb1cdd7c3d08de4a29b
jsteenb2 <jonathansteenbergen@gmail.com>
2017-05-04 23:05:11 -0500

    Add automated build using test to run the file creation on actual tribe data

--- tribe_internal_test.go
+++ tribe_internal_test.go
@@ -1,1 +1,31 @@
 package tribe
+
+import (
+	"strconv"
+	"testing"
+	"tribe/heater"
+	"tribe/parser"
+)
+
+func TestMain(t *testing.T) {
+	video := videoData{Height: 1050, Width: 1680}
+	parser.ParseFile("test.json")
+	for i, tracker := range parser.Tracker {
+		heater.CreateHeatMap(strconv.FormatInt(int64(i), 10), tracker, video)
+	}
+}
+
+type videoData struct {
+	Height int
+	Width  int
+}
+
+func (v videoData) GetHeight() uint {
+	return uint(v.Height)
+}
+
+func (v videoData) GetWidth() uint {
+	return uint(v.Width)
+}
+
+// /Users/jonathansteenbergen/async/eye_tracker/imgs/one_min_test.mp4
