d198331f6fb9e3504c3f46fceb6e4753ced582c2
jsteenb2 <jonathansteenbergen@gmail.com>
2017-05-10 20:37:52 -0500

    Add description to public facing Runner function within tribe pkg

--- tribe.go
+++ tribe.go
@@ -9,6 +9,8 @@ func main() {
 
 }
 
+// Runner is the main runner that brings in the files
+//  will be run with data receivied from user input on front end
 func Runner(video VideoInterface, jsonFilename string) {
 	parser.ParseFile(jsonFilename)
 	heater.CreateHeatMaps(parser.Tracker, video)
